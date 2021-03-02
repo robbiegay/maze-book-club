a0fea721029a3dd74f08a3ccae6f2a29083691fd
Robbie Gay <robbiecgay@gmail.com>
2021-03-02 17:46:33 -0500

    Created binary tree maze alg

--- Maze Creator/Maze Creator/Controllers/HomeController.cs
+++ Maze Creator/Maze Creator/Controllers/HomeController.cs
@@ -12,6 +12,37 @@ namespace Maze_Creator.Controllers
         public ActionResult Index()
         {
             var Model = new Maze(4, 4);
+            var rand = new Random();
+
+            for (int i = 0; i < Model.Length; i++)
+            {
+                for (int j = 0; j < Model.Width; j++)
+                {
+                    
+                    bool flipCoin = rand.NextDouble() >= 0.5;
+
+                    if (j == Model.Width - 1 && i == Model.Length - 1)
+                    {
+                        continue;
+                    }
+                    else if (j == Model.Width - 1)
+                    {
+                        Model.Grid[i][j].North = false;
+                    }
+                    else if (i == Model.Length - 1)
+                    {
+                        Model.Grid[i][j].East = false;
+                    }
+                    else if (flipCoin)
+                    {
+                        Model.Grid[i][j].North = false;
+                    }
+                    else if (!flipCoin)
+                    {
+                        Model.Grid[i][j].East = false;
+                    }
+                }
+            }
 
             return View(Model);
         }
