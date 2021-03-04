a949ff7496272ef4cc82b4b9bca8e713c62e3aa4
Robbie Gay <robbiecgay@gmail.com>
2021-03-03 21:26:15 -0500

    Migrated algorithms, interfaces, and models from .NET Framework app to .NET Core app

---
+++ RobbiesMazes/RobbiesMazes.Data/Models/Maze.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace RobbiesMazes.Data.Models
+{
+    public class Maze
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public List<List<Cell>> Grid { get; set; }
+
+        public Maze(int length, int width)
+        {
+            var grid = new List<List<Cell>>();
+
+            Length = length;
+            Width = width;
+
+            for (int i = 0; i < Length; i++)
+            {
+                var row = new List<Cell>();
+
+                for (int j = 0; j < Width; j++)
+                {
+                    row.Add(new Cell());
+                }
+
+                grid.Add(row);
+            }
+
+            Grid = grid;
+        }
+    }
+}
