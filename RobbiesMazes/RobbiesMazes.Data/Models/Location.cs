9dce0b4102d3dd6ef4fb5e1bb8740cb3363a9695
Robbie Gay <robbiecgay@gmail.com>
2021-03-04 20:24:21 -0500

    Starting on Maze Solver

---
+++ RobbiesMazes/RobbiesMazes.Data/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobbiesMazes.Data.Models
+{
+    class Location
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Location(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
---
+++ RobbiesMazes/RobbiesMazes.Data/Services/MazeSolver.cs
@@ -0,0 +1,24 @@
+﻿using RobbiesMazes.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobbiesMazes.Data.Services
+{
+    class MazeSolver
+    {
+        public Maze ActiveMaze { get; private set; }
+        public PersonInMaze Person { get; private set; }
+
+        public MazeSolver(Maze maze, PersonInMaze person)
+        {
+            ActiveMaze = maze;
+            Person = person;
+        }
+
+        public Direction FindPath()
+        {
+
+        }
+    }
+}
